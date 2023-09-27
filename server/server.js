875f1544eeb3df66997171d54ccdc7a565a39f4c
hyeiis <hyeis0807@gmail.com>
2023-09-27 09:41:35 +0900

    Set server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,23 @@
 const express = require("express");
+const db = require("./models");
 const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// router 분리
+const router = require("./routes/todo");
+app.use("/", router);
+
+// 오류 처리
+app.use("*", (req, res) => {
+  res.status(404);
+});
+
+// db 연결
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+});
